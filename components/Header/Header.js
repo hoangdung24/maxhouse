bca87ff0eff9f5d7344913c8d580d654edcb0a17
AnhPhi <phananhphi.pp@gmail.com>
2022-05-21 13:05:40 +0700

    update footer header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,19 +3,13 @@ import {
   AppBar,
   Box,
   Toolbar,
-  IconButton,
   Typography,
-  Menu,
   Container,
   Button,
   useTheme,
-  MenuItem,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
 import { keyframes } from "@emotion/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import Footer from "./Footer";
 import { useWindowScroll } from "react-use";
 import Image from "next/image";
 
@@ -33,7 +27,7 @@ const fadeIn = keyframes({
   "100%": { top: 0 },
 });
 
-const NavBars = ({ children }) => {
+const Header = ({ children }) => {
   const theme = useTheme();
   const [scroll, setScroll] = useState();
 
@@ -64,7 +58,6 @@ const NavBars = ({ children }) => {
     setScroll(y);
   });
 
-  const router = useRouter();
   const [anchorElNav, setAnchorElNav] = useState(null);
 
   const handleOpenNavMenu = (event) => {
@@ -83,7 +76,7 @@ const NavBars = ({ children }) => {
             disableGutters
             sx={{ gap: "60px", justifyContent: "center" }}
           >
-            {/* PC navBar */}
+            {/*  Header */}
             <Link href="/">
               <Image src="/img/Logo.png" width="70%" height="70%"></Image>
             </Link>
@@ -106,77 +99,10 @@ const NavBars = ({ children }) => {
                 </Link>
               ))}
             </Box>
-
-            {/* Mobile navBar */}
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages2.map((page, index) => (
-                  <MenuItem key={index} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page.name}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-
-            {/* Mobile logo */}
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href=""
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              LOGO
-            </Typography>
           </Toolbar>
         </Container>
       </AppBar>
-
-      {children}
-      {router.pathname == "/contact" ||
-      router.pathname == "/service" ||
-      router.pathname == "/news" ||
-      router.pathname == "/design" ||
-      router.pathname == "/product" ? (
-        <Footer />
-      ) : null}
     </React.Fragment>
   );
 };
-export default NavBars;
+export default Header;
